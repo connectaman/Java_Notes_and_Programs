85e4b02994239c38cf784c6c3f527e2b852df12e
connectamanulla <connectamanulla@gmail.com>
2019-07-26 11:09:35 +0530

    added many operator code

---
+++ src/Operators/LogicalOperator.java
@@ -0,0 +1,24 @@
+package Operators;
+
+public class LogicalOperator {
+    public static void main(String args[]){
+        int a=10;
+        int b=5;
+        int c=20;
+        System.out.println(a<b&&a<c);//false && true = false
+        System.out.println(a<b&a<c);//false & true = false
+    }
+    public static void Or(String args[]){
+        int a=10;
+        int b=5;
+        int c=20;
+        System.out.println(a>b||a<c);//true || true = true
+        System.out.println(a>b|a<c);//true | true = true
+//|| vs |
+        System.out.println(a>b||a++<c);//true || true = true
+        System.out.println(a);//10 because second condition is not checked
+        System.out.println(a>b|a++<c);//true | true = true
+        System.out.println(a);//11 because second condition is checked
+    }
+
+}
---
+++ src/Operators/TernaryOperator.java
@@ -0,0 +1,10 @@
+package Operators;
+
+public class TernaryOperator {
+    public static void main(String args[]){
+        int a=2;
+        int b=5;
+        int min=(a<b)?a:b;
+        System.out.println(min);  //2
+    }
+}

7bd639e450106bc1f9520483bebcbd46e86770d7
connectamanulla <connectamanulla@gmail.com>
2019-07-26 11:11:11 +0530

    operators completed

--- src/Operators/AssignmentOperator.java
+++ src/Operators/AssignmentOperator.java
@@ -9,4 +9,22 @@ public class AssignmentOperator {
         System.out.println(a);
         System.out.println(b);
     }
+    public static void Example2(String[] args){
+        int a=10;
+        a+=3;//10+3
+        System.out.println(a);
+        a-=4;//13-4
+        System.out.println(a);
+        a*=2;//9*2
+        System.out.println(a);
+        a/=2;//18/2
+        System.out.println(a);
+    }
+    public static void AddingSort(String args[]){
+        short a=10;
+        short b=10;
+        //a+=b;//a=a+b internally so fine
+        //a=a+b;//Compile time error because 10+10=20 now int
+        System.out.println(a);
+    }
 }

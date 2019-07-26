c042da791899a33d68c015dbbafb7306e733892a
connectamanulla <connectamanulla@gmail.com>
2019-07-26 11:00:39 +0530

    unary example code altered and added

---
+++ src/Operators/UnaryOperator2.java
@@ -0,0 +1,11 @@
+package Operators;
+
+public class UnaryOperator2 {
+    public static void main(String args[]){
+        int a=10;
+        int b=10;
+        System.out.println(a++ + ++a);//10+12=22
+        System.out.println(b++ + b++);//10+11=21
+
+    }
+}

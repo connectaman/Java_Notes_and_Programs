f55c600b97fef07f8885734bfe1cc1c0bdceb7c0
connectamanulla <connectamanulla@gmail.com>
2019-07-26 10:59:35 +0530

    operator and unary operator example added

---
+++ src/Operators/UnaryOperator.java
@@ -0,0 +1,20 @@
+package Operators;
+
+public class UnaryOperator {
+    public static void main(String args[]){
+        int x=10;
+        System.out.println(x++);//10 (11)
+        System.out.println(++x);//12
+        System.out.println(x--);//12 (11)
+        System.out.println(--x);//10
+    }
+}
+
+//Output
+//   10
+//   12
+//   12
+//   10
+
+
+
